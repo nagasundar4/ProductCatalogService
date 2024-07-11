98835ed9c36810a2225351ea2f1e3e64373f0921
Nagarajan Sundaram <nagasundar@gmail.com>
2024-07-11 11:04:10 +0530

    Added Calculator and Calculator Test

---
+++ src/test/java/com/example/productcatalogservice/CalculatorTest.java
@@ -0,0 +1,31 @@
+package com.example.productcatalogservice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    public void Test_AddWithTwoIntegers_RunsSuccessfully() {
+        //Arrange
+        Calculator calculator = new Calculator();
+
+        //Act
+        int result = calculator.add(21,51);
+
+        //Assert
+        assert(result==72);
+    }
+
+    @Test
+    public void Test_DivideByZero_ThrowsArithmeticException() {
+        //Arrange
+        Calculator calculator = new Calculator();
+
+        //Act
+        //int res = calculator.divide(1,0);
+        assertThrows(ArithmeticException.class,
+                () -> calculator.divide(1,0));
+    }
+}
